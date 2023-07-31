e974d320cd74038160521359369d35b76536b99f
Ganesham <ganeshamtailor@gmail.com>
2023-07-31 13:42:54 +0530

    Uploadthing working with image preview

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -27,9 +27,10 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
 
   const ref = useRef<EditorJS>();
   const [isMounted,setIsMounted]=useState<boolean>(false)
+  const _titleRef = useRef<HTMLTextAreaElement>(null)
 
   useEffect(()=>{
-    if(typeof window !== undefined){
+    if(typeof window !== 'undefined'){
       setIsMounted(true)
     }
   },[])  
@@ -71,12 +72,9 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
                   return {
                     success: 1,
                     file: {
-                      success: 1,
-                      file: {
-                        url: res.fileUrl,
-                      },
+                      url: res.fileUrl,
                     },
-                  };
+                  }
                 },
               },
             },
@@ -105,11 +103,20 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
       return() => {}
     } 
   },[isMounted, initializeEditor])
+
+  const {ref: titleRef, ...rest} = register('title')
+
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
       <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
+          ref={(e)=>{
+            titleRef(e)
+            
+            //@ts-ignore
+            _titleRef.current = e
+          }}
             placeholder="Title"
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
           />
